42e7b134272921ce3e2ac3af6bca963a65f12d27
dong hoon kim <donghoonkim1226@gmail.com>
2016-02-01 19:26:49 -0500

    Added the code and checked and made sure it was working.

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 var http = require("http");
 
+var PORTOne = 7000;
+var PORTTwo = 7500;
+
+var handleRequestGood = function (req, res) {
+  res.end("This is a good phrase " + req.url);
+}
+var handleRequestBad = function (req, res) {
+  res.end("This is a bad phrase " + req.url);
+}
+var serverGood = http.createServer(handleRequestGood);
+var serverBad = http.createServer(handleRequestBad);
+
+serverGood.listen(PORTOne, function() {
+  console.log("Server is listening at http://localhost:%s", PORTOne);
+});
+
+serverBad.listen(PORTTwo, function() {
+  console.log("Server is listening at http://localhost:%s", PORTTwo);
+});
